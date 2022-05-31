f69fbe566c6d4318e313a777a1154bab5d2f7bd5
lluis <lmonforte@cimne.upc.edu>
2022-05-31 10:22:16 +0200

    Coding just once. No copy

--- ComputeImplicitNonLinearProblemNodal.m
+++ ComputeImplicitNonLinearProblemNodal.m
@@ -331,61 +331,6 @@ end
 
 
 
-function GP = EvaluateLaw(CP, GP, consistent, RKMethod)
-
-if (GP.MCC)
-    
-    DeltaStrain = GP.StrainNew-GP.StrainPrev;
-    
-    X = [GP.StressPrev; GP.HistoryPrev];
-    
-    if (consistent)
-        if ( CP.Elastic)
-            [Xnew, D, ~] = ExplicitCamClayE(X, DeltaStrain, -1);
-        else
-            if (CP.MCC == 20)
-                [Xnew, D] = ImplicitCASM(X, DeltaStrain);
-            else
-            [Xnew, D, ~] = ExplicitCamClay2(X, DeltaStrain, -1);
-            
-            end
-        end
-    else
-        if ( RKMethod == 1)
-            RKMethod = RKMethod+1;
-        end
-        if ( CP.Elastic)
-            [Xnew, ~, D] = ExplicitCamClayE(X, DeltaStrain, RKMethod, false);
-        else
-            [Xnew, ~, D] = ExplicitCamClay2(X, DeltaStrain, RKMethod, false);
-        end
-    end
-    
-    
-    GP.StressNew  = Xnew(1:6);
-    GP.HistoryNew = Xnew(7);
-    GP.D6 = D;
-    GP.D = D([1,2,4], [1,2,4]);
-elseif (GP.VonMises)
-    DeltaStrain = GP.StrainNew-GP.StrainPrev;
-    
-    X = [GP.StressPrev];
-    
-    [Xnew, Dconsist, D] = ExplicitVonMises(X, DeltaStrain, -1);
-    if (consistent)
-        D = Dconsist;
-    end
-    
-    GP.StressNew  = Xnew(1:6);
-    
-    GP.D6 = D;
-    GP.D = D([1,2,4], [1,2,4]);
-    
-else
-    GP.StressNew = GP.StressPrev + GP.D6*(GP.StrainNew - GP.StrainPrev);
-end
-
-
 
 
 function [GPElements] = ComputeConstrainedModulus(CP, Nodes, Elements, GPElements, GPNodes)
